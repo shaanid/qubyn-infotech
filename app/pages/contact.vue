27a5e0218a21d363ae9da962a43f0a230ac6489c
shaanid <shaanid.info@gmail.com>
2026-01-26 16:27:04 +0530

    feat: design

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,10 +1,10 @@
 <script setup>
-import Stars from '~/components/Stars.vue'
-import ContactSection from '~/components/ContactSection.vue'
+import Stars from "~/components/Stars.vue";
+import ContactSection from "~/components/ContactSection.vue";
 </script>
 
 <template>
-  <div class="relative pt-32 pb-20 px-6 min-h-screen">
+  <div class="relative pt-32 pb-20 px-6 min-h-screen bg-[#020408]">
     <Stars />
     <ContactSection />
   </div>
